c95f9e97e2e03a974e3e373c681ccd17d0699df6
Mikelesnr <mwanza.n.m@gmail.com>
2024-07-28 12:23:14 +0200

    pagination implemented for projects

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,5 +1,6 @@
+import Pagination from "@/Components/Pagination";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
 
 export default function Index({ auth, projects }) {
     return (
@@ -16,22 +17,22 @@ export default function Index({ auth, projects }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            <table className="w-full text-sm text-left rt1:text-right text-gray-500 dark:text-gray-400">
+                        <div className="p-6 text-gray-900 dark:bg-gray-800">
+                            <table className="w-full text-sm text-left rt1:text-right text-gray-500 dark:text-gray-400 dark:bg-gray-800">
                                 <thead className="text-xs text-grat-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-grar-400 border-b-2 border-gray-500">
                                     <tr className="text-nowrap">
-                                        <th className="px-3 py-2">ID</th>
-                                        <th className="px-3 py-2">Image</th>
-                                        <th className="px-3 py-2">Name</th>
-                                        <th className="px-3 py-2">Status</th>
-                                        <th className="px-3 py-2">
+                                        <th className="px-3 py-3">ID</th>
+                                        <th className="px-3 py-3">Image</th>
+                                        <th className="px-3 py-3">Name</th>
+                                        <th className="px-3 py-3">Status</th>
+                                        <th className="px-3 py-3">
                                             Date Created
                                         </th>
-                                        <th className="px-3 py-2">Due Date</th>
-                                        <th className="px-3 py-2">
+                                        <th className="px-3 py-3">Due Date</th>
+                                        <th className="px-3 py-3">
                                             Created By
                                         </th>
-                                        <th className="px-3 py-2">Actions</th>
+                                        <th className="px-3 py-3">Actions</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -63,12 +64,32 @@ export default function Index({ auth, projects }) {
                                                 {project.created_by.name}
                                             </td>
                                             <td className="px-3 py-2">
-                                                {project.id}
+                                                <Link
+                                                    href={route(
+                                                        "project.edit",
+                                                        project.id
+                                                    )}
+                                                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline ma-1 mx-1"
+                                                >
+                                                    Edit
+                                                </Link>
+                                                <Link
+                                                    href={route(
+                                                        "project.destroy",
+                                                        project.id
+                                                    )}
+                                                    className="font-medium text-red-600 dark:text-red-500 hover:underline ma-1 mx-1"
+                                                >
+                                                    Delete
+                                                </Link>
                                             </td>
                                         </tr>
                                     ))}
                                 </tbody>
                             </table>
+                            <Pagination
+                                links={projects.meta.links}
+                            ></Pagination>
                         </div>
                     </div>
                 </div>
