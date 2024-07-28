1e2e2eddf48e55b2cbf109933e31ddeece3691cc
Mikelesnr <mwanza.n.m@gmail.com>
2024-07-28 13:25:33 +0200

    color coding for projecst status added

--- resources/js/constants.jsx
+++ resources/js/constants.jsx
@@ -1,21 +1,21 @@
 export const PROJECT_STATUS_CLASS_MAP = {
     pending: "bg-amber-500",
-    in_progress: "bg-blue-500 ",
+    "in-progress": "bg-blue-500 ",
     completed: "bg-green-500",
 };
 export const PROJECT_STATUS_TEXT_MAP = {
     pending: "Pending",
-    in_progress: "In Progress",
+    "in-progress": "In Progress",
     completed: "Completed",
 };
 export const TASK_STATUS_CLASS_MAP = {
     pending: "bg-amber-500",
-    in_progress: "bg-blue-500",
+    "in-progress": "bg-blue-500",
     completed: "bg-green-500",
 };
 export const TASK_STATUS_TEXT_MAP = {
     pending: "Pending",
-    in_progress: "In Progress",
+    "in-progress": "In Progress",
     completed: "Completed",
 };
 export const TASK_PRIORITY_CLASS_MAP = {
